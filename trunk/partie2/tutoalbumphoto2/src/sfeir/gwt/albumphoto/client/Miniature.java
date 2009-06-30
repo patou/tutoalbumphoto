02085b270aa406634322ce67c20d4753dcd42d0a
bilbon <bilbon@42aed80c-388f-11de-a089-7f97fea6ad66>
2009-06-30 09:46:34 +0000

    Recherche / Pages / i18n

--- trunk/partie2/tutoalbumphoto2/src/sfeir/gwt/albumphoto/client/Miniature.java
+++ trunk/partie2/tutoalbumphoto2/src/sfeir/gwt/albumphoto/client/Miniature.java
@@ -1,50 +1,62 @@
 package sfeir.gwt.albumphoto.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-
 /**
  * Composant permettant d'afficher une photo et un titre
- *
+ * 
  */
 
 public class Miniature extends Composite {
     private VerticalPanel panel;
     private Image miniature;
     private Label label;
-    
+
     /**
      * Affiche une photo miniature avec le titre centré en dessous
-     * @param url Url de la photo
-     * @param titre Titre de la photo
+     * 
+     * @param url
+     *            Url de la photo
+     * @param titre
+     *            Titre de la photo
      */
-    public Miniature(String url, String titre) {
+    public Miniature(String url, String titre, final String destination) {
         miniature = new Image(url);
+        miniature.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                Window
+                        .open(destination, "Image",
+                                "menubar=no, status=no, scrollbars=no, menubar=no, width=200, height=100");
+            }
+        });
         label = new Label(titre);
         // Centre le titre
         label.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-        // Le VerticalPanel affiche ses enfants verticalement dans l'ordre d'ajout des composants 
+        // Le VerticalPanel affiche ses enfants verticalement dans l'ordre
+        // d'ajout des composants
         panel = new VerticalPanel();
         panel.add(miniature);
         panel.add(label);
-        // Fonction importante qui indique a la classe parent quel est le composant à afficher
+        // Fonction importante qui indique a la classe parent quel est le
+        // composant à afficher
         // Initialise l'affichage
         initWidget(panel);
         // Modifie la taille du composant
         setHeight("80px");
         setWidth("80px");
-        /* Ajoute une classe CSS au code (<div class="miniature-float"></div>)
-         Cela permet de styliser notre composant
-         Ajouter dans votre fichier CSS : 
-         .miniature-float {
-            float: left;
-            margin: 20px;
-         }
-         */ 
+        /*
+         * Ajoute une classe CSS au code (<div class="miniature-float"></div>)
+         * Cela permet de styliser notre composant Ajouter dans votre fichier
+         * CSS : .miniature-float { float: left; margin: 20px; }
+         */
         setStyleName("miniature-float");
     }
 }
---
+++ trunk/partie2/tutoalbumphoto2/src/sfeir/gwt/albumphoto/client/model/Photographie.java
@@ -0,0 +1,60 @@
+package sfeir.gwt.albumphoto.client.model;
+
+import java.io.Serializable;
+
+public class Photographie implements Serializable {
+
+    private static final long serialVersionUID = -3006587958250286140L;
+
+    public String picasaUrl;
+    public String photoUrl;
+    public String photoMiniatureUrl;
+    public String photoTitre;
+
+    public Photographie() {
+
+    }
+
+    public Photographie(String picasaUrl, String photoUrl, String photoTitle) {
+        this.picasaUrl = picasaUrl;
+        this.photoUrl = photoUrl;
+        this.photoTitre = photoTitle;
+
+        int index = photoUrl.lastIndexOf("/");
+        photoMiniatureUrl = photoUrl.substring(0, index) + "/s144"
+                + photoUrl.substring(index);
+    }
+
+    public String getPicasaUrl() {
+        return picasaUrl;
+    }
+
+    public void setPicasaUrl(String picasaUrl) {
+        this.picasaUrl = picasaUrl;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getPhotoMiniatureUrl() {
+        return photoMiniatureUrl;
+    }
+
+    public void setPhotoMiniatureUrl(String photoMiniatureUrl) {
+        this.photoMiniatureUrl = photoMiniatureUrl;
+    }
+
+    public String getPhotoTitre() {
+        return photoTitre;
+    }
+
+    public void setPhotoTitre(String photoTitre) {
+        this.photoTitre = photoTitre;
+    }
+
+}
